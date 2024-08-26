d68d76d70ec2d8e0aac288686cf7fc7dc98763e3
Bullishdrake09 <97606346+Bullishdrake09@users.noreply.github.com>
2024-08-26 11:12:46 +0100

    Delete PIB Installer HERE.bat

--- PIB Installer HERE.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-%localAppData%\Programs\Python\Python312\python.exe SideJITServer\SideJITServer\__main__.py
-pip3 install --upgrade SideJITServer
-pip3 install pymobiledevice3==3.4.0
-pause
